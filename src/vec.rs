ce2ce1d53ca22f07b8c5c52c6317f7f145776914
soetl <soetl@proton.me>
2025-07-15 01:06:23 +0200

    Update dependencies and fix vector utils

--- src/vec.rs
+++ src/vec.rs
@@ -18,10 +18,12 @@ impl Random<f32> for Vec3 {
     }
 }
 
-pub(crate) trait VecExt {
+pub trait VecExt {
     fn near_zero(&self) -> bool;
     fn random_unit() -> Vec3;
-    fn random_in_unit_disk() -> Vec3;
+    fn random_unit_disk() -> Vec3;
+    fn random_hemisphere(normal: &Vec3) -> Vec3;
+    fn random_cosine_direction() -> Vec3;
 }
 
 impl VecExt for Vec3 {
@@ -40,7 +42,7 @@ impl VecExt for Vec3 {
         }
     }
 
-    fn random_in_unit_disk() -> Vec3 {
+    fn random_unit_disk() -> Vec3 {
         loop {
             let p = Vec3::new(
                 f32::random_range(&(-1.0..1.0)),
@@ -48,8 +50,29 @@ impl VecExt for Vec3 {
                 0.0,
             );
             if p.length_squared() < 1.0 {
-                return p;
+                return p.normalize();
             }
         }
+    }
+
+    fn random_hemisphere(normal: &Vec3) -> Vec3 {
+        let in_unit_sphere = Self::random_unit();
+        if in_unit_sphere.dot(*normal) > 0.0 {
+            in_unit_sphere
+        } else {
+            -in_unit_sphere
+        }
+    }
+
+    fn random_cosine_direction() -> Vec3 {
+        let r1 = f32::random();
+        let r2 = f32::random();
+        let z = (1.0f32 - r2).sqrt();
+
+        let phi = 2.0 * std::f32::consts::PI * r1;
+        let x = phi.cos() * r2.sqrt();
+        let y = phi.sin() * r2.sqrt();
+
+        Vec3::new(x, y, z)
     }
 }
